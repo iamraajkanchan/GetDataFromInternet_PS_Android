880049f9aa4e69b3f1994daf927d22947c58e3bd
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-08-10 18:43:36 +0530

    MainActivity is bounded with layout using viewBinding.

--- app/src/main/java/com/chinkiandfamily/getdatafrominternet/MainActivity.kt
+++ app/src/main/java/com/chinkiandfamily/getdatafrominternet/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import com.chinkiandfamily.getdatafrominternet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityMainBinding
+    private val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        if (!this::_binding.isInitialized) {
+            setContentView(binding.root)
+        }
     }
 }
