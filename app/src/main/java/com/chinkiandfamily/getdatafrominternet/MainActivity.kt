f5b51f8b50015af02e22a4a230d2a7bdd9a5754b
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-08-10 18:38:55 +0530

    Initial commit

---
+++ app/src/main/java/com/chinkiandfamily/getdatafrominternet/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chinkiandfamily.getdatafrominternet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chinkiandfamily.getdatafrominternet.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
